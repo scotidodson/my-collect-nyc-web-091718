4dcfa0d84a4df6594ef6b3c5307f4bf8fe208381
Scoti Dodson <scotidodson@gmail.com>
2018-09-05 21:45:26 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
-
+def my_collect(collection)
+  if block_given?
+  i = 0 
+  new_collection = []
+  
+    while i < collection.length 
+    new_collection << yield(collection[i])
+    i = i + 1 
+    end
+    new_collection
+  else
+    "Need an argument."
+  end
+end 
 
